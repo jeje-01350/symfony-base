1ad53778b139fae4d689f4a804edb54dbe3c8196
jeje-01350 <jeremyrossi1234@gmail.com>
2021-11-26 10:40:31 +0100

    initial

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function index(): Response
+    {
+
+        $form = $this->createForm(ContactType::class);
+
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/ContactType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('email', EmailType::class)
+            ->add('subject', TextType::class, [
+                'label' => 'Objet'
+            ])
+            ->add('message', TextareaType::class)
+            ->add('submit', SubmitType::class,[
+                'label'=>'Envoyer Le Message',
+                'attr' => [
+                    'style' => 'width:100%;'
+                ]
+            ])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+        ]);
+    }
+}
