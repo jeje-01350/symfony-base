77a429ca1c89e38a048e25ba694d7478b18d9c5c
jeje-01350 <jeremyrossi1234@gmail.com>
2021-12-02 20:07:47 +0100

    alert

---
+++ assets/js/alert.js
@@ -0,0 +1,7 @@
+$("#success-alert").fadeTo(2000, 500).slideUp(500, function(){
+    $("#success-alert").slideUp(500);
+});
+
+$("#error-alert").fadeTo(2000, 500).slideUp(500, function(){
+    $("#error-alert").slideUp(500);
+});
--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,7 @@ import 'jquery'
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
+import '../js/alert';
 // any CSS you import will output into a single css file (app.css in this case)
 import '../styles/app.css';
 
